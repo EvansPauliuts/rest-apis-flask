41a2112e0a625a266ec967b98dc3da7d6fd9efce
Evans <pauliutsevans@gmail.com>
2022-12-28 19:36:31 +0300

    Update alembic

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask, jsonify
 from flask_smorest import Api
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
+from dotenv import load_dotenv
 
 from db import db
 from blocklist import BLOCKLIST
@@ -16,6 +17,8 @@ from resources.user import blp as UserBlueprint
 
 def create_app(db_url=None):
     app = Flask(__name__)
+
+    load_dotenv()
 
     app.config['PROPAGATE_EXCEPTIONS'] = True
     app.config['API_TITLE'] = 'Stores REST API'
